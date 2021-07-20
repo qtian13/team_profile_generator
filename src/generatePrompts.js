025a1c1767aa415ee767b9be122ea944b33f2838
qtian13 <qiushuang.tian@gmail.com>
2021-07-19 23:59:20 -0700

    remove repetative code and make the code clean and easy to maintainance

--- src/generatePrompts.js
+++ src/generatePrompts.js
@@ -4,169 +4,129 @@ const Intern = require('../lib/Intern');
 const Manager = require('../lib/Manager');
 const generateCard = require('./cardGenerator');
 const generateHtml = require('./generateHtml');
-let employees = [];
 let cardsSnippet = ``;
 
-const questionsAboutManager = [
-    {
-        type: 'input',
-        message: "What is the team manager's name?",
-        name: 'name',
-    },
-    {
-        type: 'input',
-        message: "What is the team manager's id?",
-        name: 'id',
-        validate(value) {
-            const valid = !isNaN(parseInt(value));
-            return valid || 'Please enter a number';
-        },
-        // filter: Number,
-    },
-    {
-        type: 'input', 
-        message: "What is the team manager's email?",
-        name: 'email',
-    },
-    {
-        type: 'input',
-        message: "What is the team manager's office number?",
-        name: 'officeNumber',
-        validate(value) {
-            const valid = !isNaN(parseInt(value));
-            return valid || 'Please enter a number';
-        },
-    },
-    {
-        type: 'list',
-        message: 'Which type of team member do you want to add?',
-        name: 'nextRole',
-        choices: ['Engineer', 'Intern', 'I do not want to add any more team members.'],
+const commonQuestions = (role) => {
+    let phrase;
+    switch (role) {
+        case "Manager":
+            phrase = "the team manager";
+            break;
+        case "Engineer":
+            phrase = "your engineer";
+            break;
+        case "Intern":
+            phrase = "your intern";
+            break;
+        default:
+            phrase = "";
     }
-];
-const questionsAboutEngineer = [
-    {
-        type: 'input',
-        message: "What is your engineer's name?",
-        name: 'name',
-    },
-    {
-        type: 'input',
-        message: "What is your engineer's id?",
-        name: 'id',
-        validate(value) {
-            const valid = !isNaN(parseInt(value));
-            return valid || 'Please enter a number';
+    return [
+        {
+            type: 'input',
+            message: "What is " + phrase + "'s name?",
+            name: 'name',
         },
-        // filter: Number,
-    },
-    {
-        type: 'input', 
-        message: "What is your engineer's email?",
-        name: 'email',
-    },
-    {
-        type: 'input',
-        message: "What is your engineer's GitHub username?",
-        name: 'github',
-    },
-    {
-        type: 'list',
-        message: 'Which type of team member do you want to add?',
-        name: 'nextRole',
-        choices: ['Engineer', 'Intern', 'I do not want to add any more team members.'],
-    }
-];
-const questionsAboutIntern = [
-    {
-        type: 'input',
-        message: "What is your intern's name?",
-        name: 'name',
-    },
-    {
-        type: 'input',
-        message: "What is your intern's id?",
-        name: 'id',
-        validate(value) {
-            const valid = !isNaN(parseInt(value));
-            return valid || 'Please enter a number';
+        {
+            type: 'input',
+            message: "What is " + phrase + "'s id?",
+            name: 'id',
+            validate(value) {
+                const valid = !isNaN(parseInt(value));
+                return valid || 'Please enter a number';
+            },
+            // filter: Number,
         },
-        // filter: Number,
-    },
-    {
-        type: 'input', 
-        message: "What is your intern's email?",
-        name: 'email',
-    },
-    {
-        type: 'input',
-        message: "What is your intern's school?",
-        name: 'school',
-    },
-    {
-        type: 'list',
-        message: 'Which type of team member do you want to add?',
-        name: 'nextRole',
-        choices: ['Engineer', 'Intern', 'I do not want to add any more team members.'],
-    }
-];
+        {
+            type: 'input', 
+            message: "What is " + phrase + "'s email?",
+            name: 'email',
+        }
+    ]
+};
 
-function promptManager() {
+const specialQuesion = (role) => {
+    switch (role) {
+        case "Manager":
+            return {
+                type: 'input',
+                message: "What is the team manager's office number?",
+                name: 'officeNumber',
+                validate(value) {
+                    const valid = !isNaN(parseInt(value));
+                    return valid || 'Please enter a number';
+                },
+            };
+        case "Engineer":
+            return {
+                type: 'input',
+                message: "What is your engineer's GitHub username?",
+                name: 'github',
+            };
+        case "Intern":
+            return {
+                type: 'input',
+                message: "What is your intern's school?",
+                name: 'school',
+            };
+        default:
+            return {};
+    }
+}
+
+const questions = (role) => {
+    return [
+        ...commonQuestions(role),
+        specialQuesion(role),
+        {
+            type: 'list',
+            message: 'Which type of team member do you want to add?',
+            name: 'nextRole',
+            choices: ['Engineer', 'Intern', 'I do not want to add any more team members.'],
+        }
+    ];
+}
+
+const promptQuestions = (role) => {
     inquirer
-        .prompt(questionsAboutManager)
+        .prompt(questions(role))
         .then((answer) => {
-            const {name, id, email, officeNumber} = answer;
-            const manager = new Manager(name, id, email, officeNumber);
-            cardsSnippet += generateCard(manager);
-            if (answer.nextRole === 'Engineer') {
-                promptEngineer();
-            } else if (answer.nextRole === 'Intern') {
-                promptIntern();
+            switch (role) {
+                case "Manager": {
+                    const {name, id, email, officeNumber} = answer;
+                    const manager = new Manager(name, id, email, officeNumber);
+                    cardsSnippet += generateCard(manager);
+                    break;
+                }
+                case "Engineer": {
+                    const {name, id, email, github} = answer;
+                    const engineer = new Engineer(name, id, email, github);
+                    cardsSnippet += generateCard(engineer);
+                    break;
+                }
+                case "Intern": {
+                    const {name, id, email, school} = answer;
+                    const intern = new Intern(name, id, email, school);
+                    cardsSnippet += generateCard(intern);
+                    break;
+                }
+                default:
+                    break;
+            }
+
+            if (answer.nextRole === 'Engineer' || answer.nextRole === 'Intern') {
+                promptQuestions(answer.nextRole);
             } else {
                 generateHtml(cardsSnippet);
             }
         })
 }
 
-function promptEngineer() {
-    inquirer
-        .prompt(questionsAboutEngineer)
-        .then((answer) => {
-            const {name, id, email, github} = answer;
-            const engineer = new Engineer(name, id, email, github);
-            cardsSnippet += generateCard(engineer);
-            if (answer.nextRole === 'Engineer') {
-                promptEngineer();
-            } else if (answer.nextRole === 'Intern') {
-                promptIntern();
-            } else {
-                generateHtml(cardsSnippet);
-            }
-    })
-}
-
-function promptIntern() {
-    inquirer
-        .prompt(questionsAboutIntern)
-        .then((answer) => {
-            const {name, id, email, school} = answer;
-            const intern = new Intern(name, id, email, school);
-            cardsSnippet += generateCard(intern);
-            if (answer.nextRole === 'Engineer') {
-                promptEngineer();
-            } else if (answer.nextRole === 'Intern') {
-                promptIntern();
-            } else {
-                generateHtml(cardsSnippet);
-            }
-        })
-}
-
-function generatePrompts() {
+const generatePrompts = () => {
     employees = [];
     cardsSnippet = ``;
-    promptManager();
-}
+    promptQuestions("Manager");
+};
 
 module.exports = {
     generatePrompts,

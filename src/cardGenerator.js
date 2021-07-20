341ec9c2f85656ecdcaca3976189cc24cb7b353e
qtian13 <qiushuang.tian@gmail.com>
2021-07-20 00:55:50 -0700

    code refactoring and css style

--- src/cardGenerator.js
+++ src/cardGenerator.js
@@ -1,30 +1,45 @@
-function generateCard(employeeAdded) {
-    let specialPanel;
+function generateSpecialPanel(employeeAdded) {
     switch (employeeAdded.getRole()) {
         case "Manager":
-            specialPanel = `<div class="panel-block">
+            return `<div class="panel-block">
                         OfficeNumber: ${employeeAdded.getOfficeNumber()}
                     </div>`
-            break;
         case "Engineer":
-            specialPanel = `<a class="panel-block" href="https://github.com/${employeeAdded.getGithub()}" target="_blank">
+            return `<a class="panel-block" href="https://github.com/${employeeAdded.getGithub()}" target="_blank">
                         GitHub: ${employeeAdded.getGithub()}
                     </a>`
-            break;
         case "Intern":
-            specialPanel = `<div class="panel-block">
+            return `<div class="panel-block">
                         School: ${employeeAdded.getSchool()}
                     </div>`
-            break;
         default:
-            specialPanel= ``;
+            return ``;
     }
+}
+
+function generateIcon(role) {
+    switch (role) {
+        case "Manager":
+            return `<i class="fas fa-mug-hot"></i>`;
+        case "Engineer":
+            return `<i class="fas fa-glasses"></i>`
+        case "Intern":
+            return `<i class="fas fa-user-graduate"></i>`
+        default:
+            return ``;
+    }
+}
+
+function generateCard(employeeAdded) {
+    const role = employeeAdded.getRole();
     return `
     <div class="column is-4">
-        <div class="card" style="height: 100%;">
+        <div class="card">
             <div class="card-content">
-                <div class="title">${employeeAdded.name}</div>
-                <div class="subtitle">${employeeAdded.getRole()}</div>
+                <div class="card-title">
+                    <div class="title">${employeeAdded.name}</div>
+                    <div class="subtitle"><span class="icon">${generateIcon(role)}</span>${employeeAdded.getRole()}</div>
+                </div>
                 <div class="panel">
                     <div class="panel-block">
                         ID: ${employeeAdded.getId()}
@@ -32,7 +47,7 @@ function generateCard(employeeAdded) {
                     <a class="panel-block" href="mailto:${employeeAdded.getEmail()}" target="_blank">
                         Email: ${employeeAdded.getEmail()}
                     </a>
-                    ${specialPanel}
+                    ${generateSpecialPanel(employeeAdded)}
                 </div>
             </div>
         </div>
--- src/generateHtml.js
+++ src/generateHtml.js
@@ -8,6 +8,7 @@ function generateHtml(cardsContent) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <link rel="stylesheet" href="./style.css">
     <title>Team Profile Generator</title>
 </head>
@@ -17,13 +18,12 @@ function generateHtml(cardsContent) {
             <h1 class="title has-text-centered">Team Profile</h1>
         </div>
     </header>
-    <main style="max-width: 1000px; margin-left: auto; margin-right:auto">
+    <main>
         <div class="columns is-flex is-flex-wrap-wrap">
             ${cardsContent}
         </div>
     </main>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="../src/generatePrompts.js"></script>
 </body>
 </html>`;
     writeToFile("./dist/index.html", html);

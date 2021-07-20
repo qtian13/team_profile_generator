8e9c9ca6b4b5db80f1a16aeebb2da814337311c8
qtian13 <qiushuang.tian@gmail.com>
2021-07-19 22:19:26 -0700

    pass all the tests for Employee's properties and functions

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,51 +1,74 @@
 const Employee = require('../lib/Employee');
 
-describe('Employee', () => {
-    it('should return an object of Employee', () => {
+describe("Employee", () => {
+    it("should return an object containing properties 'name', 'id', 'email' when called with the 'new' keyword", () => {
+        // Act
+        const obj = new Employee();
+
         // Assert
-        const employee = new Employee();
-        expect(employee).toBe('object');
+        expect("name" in obj).toEqual(true);
+        expect("id" in obj).toEqual(true);
+        expect("email" in obj).toEqual(true);
     });
 
-    it('should return the name set', () => {
-        // Assert
+    it("should set 'name' when created", () => {
+        // Arrange
         const name = "Sandy";
-        const employee = new Employee(name);
-        expect(employee.name).toBe('Sandy');
+        // Act
+        const obj = new Employee(name);
+        // Assert
+        expect(obj.name).toEqual("Sandy");
     });
 
-    it('should return the id set', () => {
-        // Assert
+    it("should set 'id' when created", () => {
+        // Arrange
         const id = 999;
-        const employee = new Employee("Foo", id);
-        expect(employee.id).toBe(id);
+        // Act
+        const obj = new Employee("Foo", id);
+        // Assert
+        expect(obj.id).toEqual(id);
     });
 
-    it('should return email address set', () => {
-        // Assert
+    it("should set 'email' when created", () => {
+        // Arrange
         const email = "foo@gmail.com";
-        const employee = new Employee("Foo", 0, email);
-        expect(employee.email).toBe(email);
+        // Act
+        const obj = new Employee("Foo", 0, email);
+        // Assert
+        expect(obj.email).toEqual(email);
     });
 
-    it('should return name of the object', () => {
-        // Assert
+    it("should return the 'Employee' object's 'name' value", () => {
+        // Arrange
         const name = "Sandy";
-        const employee = new Employee(name);
-        expect(employee.getName()).toBe('Sandy');
+        // Act
+        const obj = new Employee(name);
+        // Assert
+        expect(obj.getName()).toEqual(name);
     });
 
-    it('should return the id set', () => {
-        // Assert
+    it("should return the 'Employee' object's 'id' value", () => {
+        // Arrange
         const id = 999;
-        const employee = new Employee("Foo", id);
-        expect(employee.getId()).toBe(id);
+        // Act
+        const obj = new Employee("Foo", id);
+        // Assert
+        expect(obj.getId()).toEqual(id);
     });
 
-    it('should return the email set', () => {
-        // Assert
+    it("should return the 'Employee' object's 'email' value", () => {
+        // Arrange
         const email = "foo@gmail.com";
-        const employee = new Employee("Foo", 0, email);
-        expect(employee.getEmail).toBe(email);
+        // Act
+        const obj = new Employee("Foo", 0, email);
+        // Assert
+        expect(obj.getEmail()).toEqual(email);
+    });
+
+    it("should return 'Employee'", () => {
+        // Act
+        const obj = new Employee();
+        // Assert
+        expect(obj.getRole()).toEqual("Employee");
     });
 });
